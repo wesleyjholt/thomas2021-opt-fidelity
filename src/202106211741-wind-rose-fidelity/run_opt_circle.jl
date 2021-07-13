74101f779907ba0583b8af94d502773d1b16da66
Wesley Holt <wesleyjholt@gmail.com>
2021-07-13 17:47:20 -0600

    add distrubuted processing capability to circle opt script

--- src/202106211741-wind-rose-fidelity/run_opt_circle.jl
+++ src/202106211741-wind-rose-fidelity/run_opt_circle.jl
@@ -2,6 +2,8 @@ using SNOW
 using Snopt
 using PyPlot
 using OrderedCollections
+using Distributed
+using ClusterManagers
 
 plotresults = true
 verbose = true
@@ -11,6 +13,8 @@ verbose = true
 ##########################################################
 
 ### parse input arguments ###
+
+ntasks = parse(Int, ENV["SLURM_NTASKS"])
 
 if length(ARGS)<1
     # defaults
@@ -27,24 +31,24 @@ diameter_spacing = parse(Int,ARGS[6])
 ### set input file path/names ###
 
 # farm name
-farm_name = "circle-$(nturbines)turb-$(diameter_spacing)diam"
+@everywhere farm_name = "circle-$($nturbines)turb-$($diameter_spacing)diam"
 # farm definition
-path_to_farm_definition_directory = "../inputfiles/farms/random-layouts/$farm_name/"
-farm_definition_filename = "initial-design-$layout_number.yaml"
+@everywhere path_to_farm_definition_directory = "../inputfiles/farms/random-layouts/$($farm_name)/"
+@everywhere farm_definition_filename = "initial-design-$($layout_number).yaml"
 # wind resource
-path_to_wind_resource_directory = "../inputfiles/wind/wind-rose-fidelity/horns-rev/"
+@everywhere path_to_wind_resource_directory = "../inputfiles/wind/wind-rose-fidelity/horns-rev/"
 if parse(Int64,nspeeds)==1
-    wind_resource_filename = "hornsrev-windresource-$(ndirs)dirs-averagespeeds.yaml"
+    @everywhere wind_resource_filename = "hornsrev-windresource-$($ndirs)dirs-averagespeeds.yaml"
 else
-    wind_resource_filename = "hornsrev-windresource-$(ndirs)dirs-$(nspeeds)speeds.yaml"
+    @everywhere wind_resource_filename = "hornsrev-windresource-$($ndirs)dirs-$($nspeeds)speeds.yaml"
 end
 # flow models
-path_to_flow_models_directory = "../inputfiles/model-sets/flow-models/"
-flow_models_filename = "$wake_model-NoLocalTI.yaml"
+@everywhere path_to_flow_models_directory = "../inputfiles/model-sets/flow-models/"
+@everywhere flow_models_filename = "$($wake_model)-NoLocalTI.yaml"
 
 ### import problem setup file ###
 
-include("problem_setup_circle.jl")
+@everywhere include("problem_setup_circle.jl")
 
 
 ##########################################################
@@ -54,7 +58,7 @@ include("problem_setup_circle.jl")
 ### set up optimization ###
 
 # get SNOW arguments
-func!, x0, ng, lx, ux, lg, ug = ff.get_SNOW_optimization_inputs(wind_farm_opt_problem)
+@everywhere func!, x0, ng, lx, ux, lg, ug = ff.get_SNOW_optimization_inputs(wind_farm_opt_problem)
 
 # test objective and constraint functions
 nx = length(x0)
